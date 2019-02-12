692151b0f4d2ddc5145d62bf0ca0dc6d976fbf04
Helge Eichhorn <git@helgeeichhorn.de>
2019-02-12 06:53:33 +0100

    Fix config

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,3 @@ use Mix.Config
 config :julex, Julex.Worker,
   project: "test/TestJulex",
   module: "TestJulex"
-
-import_config "#{Mix.env()}.exs"
--- config/test.exs
+++
@@ -1,5 +0,0 @@
-use Mix.Config
-
-config :julex, Julex.Worker,
-  project: "test/TestJulex",
-  module: "TestJulex"
